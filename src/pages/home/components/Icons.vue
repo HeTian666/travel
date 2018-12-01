3c0dd4597b76644adf6faa9e9895b5ce25aa44dc
a4638966 <2971714942@qq.com>
2018-12-01 16:16:35 +0800

    '添加列表图标代码'

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="warp">
+  <div class="waper">
     <div class="icon">
       <div class="icon-img">
-        <img class="icon-img-content" src="">
+        <img class="icon-img-content" src="//s.qunarzz.com/vacation_react/freetrip/local.png">
       </div>
-      <p class="icon-desc"></p>
+      <p class="icon-desc">热门景点</p>
     </div>
   </div>
 </template>
@@ -16,5 +16,37 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '~styles/varibles.styl'
+  .waper
+    overflow: hidden
+    width:100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%;
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: 0.44rem
+        padding: 0.1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: 0.44rem
+        line-height 0.44rem
+        text-align: center
+        color: $darkTextColor
 </style>
