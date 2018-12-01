c04d745934f552de841e91a3ee8f75fa9d878aaa
a4638966 <2971714942@qq.com>
2018-12-01 16:00:11 +0800

    '添加icons文件'

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="warp">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="">
+      </div>
+      <p class="icon-desc"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
