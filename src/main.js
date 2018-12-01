0a7acab6d54fd04cf3323c53fef688a23c541469
a4638966 <2971714942@qq.com>
2018-12-01 14:24:18 +0800

    '修改webpack配置，简化文件引用路径'

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 // 解决移动端部分机型300毫秒点击延迟的问题
