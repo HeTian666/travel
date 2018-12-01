f28e1dd985707b8333e0df4f5838afd415989aba
a4638966 <2971714942@qq.com>
2018-12-01 15:33:37 +0800

    '添加并编写轮播图组件'

--- src/main.js
+++ src/main.js
@@ -4,13 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+// 引入vue-awesome-swiper轮播组件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+// 引入vue-awesome-swiper轮播css样式
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 解决移动端部分机型300毫秒点击延迟的问题
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="warp">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        autoplay: 2000,
+        speed: 1000,
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/c7/9e447c013b556902.jpg_890x330_c74c8ce5.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/d8/2977624617721802.jpg_890x330_39ab9791.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .warp >>> .swiper-pagination-bullet-active
+    background: #fff
+  .warp
+    overflow: hidden
+    width: 100%
+    height: 0
+    background: #333
+    padding-bottom: 37.07%
+    .swiper-img
+      width: 100%
+</style>
