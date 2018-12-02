931aa2637f91d4219d14b4af53a2a99952746784
a4638966 <2971714942@qq.com>
2018-12-02 22:43:37 +0800

    '完成icons组件功能编写'

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,52 +1,122 @@
 <template>
   <div class="waper">
-    <div class="icon">
-      <div class="icon-img">
-        <img class="icon-img-content" src="//s.qunarzz.com/vacation_react/freetrip/local.png">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIcons'
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {
+        autoplay: false
+      },
+      iconLists: [
+        {
+          id: '0001',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0002',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0003',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0004',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0005',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0006',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0007',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0008',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        },
+        {
+          id: '0009',
+          imgUrl: '//s.qunarzz.com/vacation_react/freetrip/local.png',
+          desc: '热门景点'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconLists.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
-  .waper
-    overflow: hidden
+  .waper >>> .swiper-container
     width:100%
     height: 0
     padding-bottom: 50%
-    .icon
-      position: relative
-      overflow: hidden
-      float: left
-      width: 25%;
-      height: 0
-      padding-bottom: 25%
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: 0.44rem
-        padding: 0.1rem
-        .icon-img-content
-          display: block
-          margin: 0 auto
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: 0.44rem
-        line-height 0.44rem
-        text-align: center
-        color: $darkTextColor
+  .icon
+    position: relative
+    overflow: hidden
+    float: left
+    width: 25%;
+    height: 0
+    padding-bottom: 25%
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 0.44rem
+      padding: 0.1rem
+      .icon-img-content
+        display: block
+        margin: 0 auto
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: 0.44rem
+      line-height 0.44rem
+      text-align: center
+      color: $darkTextColor
 </style>
