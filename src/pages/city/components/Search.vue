62fdbb0156dbdc5c5d7d6f917105be04ad320d9f
qi <948017056@qq.com>
2018-12-10 18:04:43 +0800

    添加部分省市搜索代码

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市或拼音">
+  <div>
+    <div class="search">
+      <input class="search-input" type="text" placeholder="输入城市或拼音" v-model="keyword">
+    </div>
+    <div class="search-content">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">
+          {{ item.name }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  proos: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        console.log(this.cities)
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 
@@ -25,4 +60,18 @@ export default {
       color: #666
       padding: 0 0.1rem
       box-sizing: border-box
+  .search-content
+    z-index: 1
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    background: #eee
+    .search-item
+      line-height: 0.62rem
+      padding-left: 0.2rem
+      background: #fff
+      color: #666
 </style>
