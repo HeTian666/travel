084bffd32cd192d7345493695902e8667f31a73f
qi <948017056@qq.com>
2018-12-10 18:22:00 +0800

    完善城市搜索功能

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,20 +3,22 @@
     <div class="search">
       <input class="search-input" type="text" placeholder="输入城市或拼音" v-model="keyword">
     </div>
-    <div class="search-content">
+    <div class="search-content" v-show="keyword" ref="search">
       <ul>
         <li class="search-item border-bottom" v-for="item of list" :key="item.id">
           {{ item.name }}
         </li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配的城市</li>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
-  proos: {
+  props: {
     cities: Object
   },
   data () {
@@ -25,6 +27,14 @@ export default {
       list: [],
       timer: null
     }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
   },
   watch: {
     keyword () {
@@ -37,7 +47,13 @@ export default {
       }
       this.timer = setTimeout(() => {
         const result = []
-        console.log(this.cities)
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.value) > -1) {
+              result.push(value)
+            }
+          })
+        }
         this.list = result
       }, 100)
     }
